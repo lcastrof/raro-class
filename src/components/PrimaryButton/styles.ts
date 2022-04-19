73944ebdbe6f2d65b0979067b6073bf6fd85f50a
Lucas de Castro <lucascastrofr@gmail.com>
2022-04-19 19:04:38 -0300

    style: muda peso e corrige fonte no botão

--- src/components/PrimaryButton/styles.ts
+++ src/components/PrimaryButton/styles.ts
@@ -19,6 +19,7 @@ export const Button = styled.button<ButtonProps>`
       : theme.spacing.medium};
     color: ${theme.colors.white};
     font-size: ${theme.font.sizes.small};
+    font-weight: ${theme.font.weight.semiBold};
   `}
 
   &:hover {
