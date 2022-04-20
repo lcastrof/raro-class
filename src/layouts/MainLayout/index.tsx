d7c92612fe62e42caa6222e4ae154d870b107314
Lucas de Castro <lucascastrofr@gmail.com>
2022-04-19 22:36:34 -0300

    refactor: ajusta main layout

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../../components/Header';
+import { Footer } from '../../components/Footer';
 import * as S from './styles';
 
 export const MainLayout = () => (
-  <>
+  <S.Container>
     <Header />
-    <S.MainContent>
-      <Outlet />
-    </S.MainContent>
-  </>
+    <S.BellowWrapper>
+      <S.MainContent>
+        <Outlet />
+      </S.MainContent>
+      <Footer />
+    </S.BellowWrapper>
+  </S.Container>
 );
