071a68fdcf9b783d27d11291b754d67c6eda691c
Lucas de Castro <lucascastrofr@gmail.com>
2022-04-21 13:23:33 -0300

    feat: cria componente de título para as seções

---
+++ src/components/RowTitle/index.tsx
@@ -0,0 +1,24 @@
+import * as S from './styles';
+
+type RowTitleProps = {
+  title: string;
+  type?: 'class' | 'favorite';
+  level?: 1 | 2;
+};
+
+export const RowTitle: React.FC<RowTitleProps> = ({
+  title,
+  type = 'class',
+  level = 2
+}) => {
+  const iconSrc =
+    type === 'class'
+      ? '/assets/art/artIcon-logoHatRaroAcademy.svg'
+      : '/assets/art/artIcon-starPreenchida.svg';
+  return (
+    <S.Container>
+      <S.Icon src={iconSrc} />
+      <S.Title as={level === 1 ? 'h1' : 'h2'}>{title}</S.Title>
+    </S.Container>
+  );
+};
