aefa59cc1992cb11f0c54d2264e44d12b940ed13
Denis Araujo <denisgomes938@gmail.com>
2022-04-21 17:00:38 -0300

    finalizado pagina de Login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,18 +1,28 @@
 import { Input } from '../../components/Input';
 import { AiOutlineUser, AiOutlineEyeInvisible } from 'react-icons/ai';
 import { HiOutlineKey } from 'react-icons/hi';
-import { PageMain } from '../../layout/PageMain';
+import { PageMain } from '../../layouts/PageMain';
+import { PrimaryButton } from '../../components/PrimaryButton';
+import { MainTextLayout } from '../../layouts/MainTextLayout';
 
 export const Login: React.FC = () => {
   return (
     <PageMain>
-      <Input placeHolder="E-mail" icon={<AiOutlineUser size={18} />} />
-      <Input
-        placeHolder="senha"
-        icon={<HiOutlineKey size={18} />}
-        iconRight={<AiOutlineEyeInvisible size={18} />}
-        type="password"
-      />
+      <MainTextLayout
+        title="Conecte-se"
+        subTitle="Venha ser Raro"
+        linkparagrafo1="Esqueci minha senha"
+        linkparagrafo2="Criar Conta"
+      >
+        <Input placeHolder="E-mail" icon={<AiOutlineUser size={18} />} />
+        <Input
+          placeHolder="Senha"
+          icon={<HiOutlineKey size={18} />}
+          iconRight={<AiOutlineEyeInvisible size={18} />}
+          type="password"
+        />
+        <PrimaryButton size="fullWidth" text="Login" />
+      </MainTextLayout>
     </PageMain>
   );
 };
