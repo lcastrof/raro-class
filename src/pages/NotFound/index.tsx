9de6ffaa3a5db4ef28ebebd71b3662b2c7d636af
Francielle Zucolota <fran.zucolota@hotmail.com>
2022-04-21 02:17:32 -0300

    refactor: import link from react-router

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -4,12 +4,10 @@ import { Link } from 'react-router-dom';
 const NotFound = () => {
   return (
     <S.Container>
-      <Link to="/">
-        <S.LogoImage
-          alt="Logo Raro Academy. Com ícone de capelo e escrito Raro Academy"
-          src="/assets/logo/logoHorizontal-raroAcademy-white.svg"
-        />
-      </Link>
+      <S.LogoImage
+        alt="Logo Raro Academy. Com ícone de capelo e escrito Raro Academy"
+        src="/assets/logo/logoHorizontal-raroAcademy-white.svg"
+      />
       <S.BackgroundArt src="/assets/art/artBackgroundNotFoundPage.png" />
       <S.WrapText>
         <h1>404</h1>
