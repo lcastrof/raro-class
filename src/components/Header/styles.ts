b26642fd8802faad3e8ee7b2bf76565b26e3387a
Francielle Zucolota <fran.zucolota@hotmail.com>
2022-04-27 18:47:55 -0300

    refactorStyle: footer e header para a page video class

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,7 +1,7 @@
 import styled, { css } from 'styled-components';
 
 export const HeaderContainer = styled.header`
-  height: 9.3rem;
+  height: 5.3rem;
   width: 100%;
   display: flex;
   align-items: center;
@@ -11,7 +11,7 @@ export const HeaderContainer = styled.header`
     box-shadow: ${theme.boxShadow.header};
     background-color: ${theme.colors.white};
     padding-inline: ${theme.spacing.huge};
-    padding-block: ${theme.spacing.medium};
+    padding-block: ${theme.spacing.xsmall};
   `}
 `;
 
--- src/components/UserDropdown/styles.ts
+++ src/components/UserDropdown/styles.ts
@@ -2,6 +2,7 @@ import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
   position: absolute;
+  z-index: 1;
   right: 0;
   top: 6rem;
   display: flex;
