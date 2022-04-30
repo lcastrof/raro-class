21d913f8198eb39760f9817a9a4dda196e4a8da6
Lucas de Castro <lucascastrofr@gmail.com>
2022-04-30 18:03:04 -0300

    refactor: começa refatoração da página de video

--- src/pages/VideoClass/index.tsx
+++ src/pages/VideoClass/index.tsx
@@ -8,37 +8,36 @@ import DivideLine from '../../components/DivideLine';
 import { CommentList } from '../../components/CommentsList';
 import { VideoRecomendedList } from '../../components/VideoRecomendedList';
 import { SkeletonColunmListVideos } from '../../components/Skeleton/SkeletonColunmListVideos';
+import { formataDate } from '../../helpers/date';
+import { FavoriteButton } from '../../components/FavoriteButton';
+import { Video } from '../../types/Video';
 
-export type InfoVideoClass = {
+type Aluno = {
   id: string;
+  admin: boolean;
   nome: string;
-  url: string;
-  thumbUrl: string;
-  descricao: string;
-  createdAt: string;
-  duracao: string;
-  dataPublicacao?: string | null;
-  topico: string;
-  tags: string[];
+  email: string;
+  senha: string;
+  foto: string;
+};
+
+export type Comment = {
+  id: string;
   texto: string[];
-  aluno: {
-    id: string;
-    admin: boolean;
-    nome: string;
-    email: string;
-    senha: string;
-    foto: string;
-  };
+  aluno: Aluno;
   upVotes: number;
   downVotes: number;
+  meuVote: {
+    id: string;
+    vote: 'up' | 'down';
+    aluno: Aluno;
+  };
 };
 
 export const VideoClass = () => {
-  const [recommendedVideos, setRecommendedVideos] = useState<InfoVideoClass[]>(
-    []
-  );
+  const [recommendedVideos, setRecommendedVideos] = useState<Video[]>([]);
 
-  const [comments, setComments] = useState<InfoVideoClass[]>([]);
+  const [comments, setComments] = useState<Comment[]>([]);
 
   const [loading, setLoading] = useState(true);
 
@@ -46,7 +45,8 @@ export const VideoClass = () => {
 
   const [videoUrl, setVideoUrl] = useState();
   const [videoThumbnail, setVideoThumbnail] = useState();
-  const [videoApi, setVideoApi] = useState<InfoVideoClass>();
+  const [videoApi, setVideoApi] = useState<Video>();
+  console.log({ videoApi });
 
   useEffect(() => {
     async function loadVideo() {
@@ -73,74 +73,71 @@ export const VideoClass = () => {
 
   if (loading) {
     return (
-      <S.Container2>
+      <S.Container>
         <S.ContainerPLayerVideo>
           <SkeletonPlayerVideo />
         </S.ContainerPLayerVideo>
         <S.ContainerRight>
           <SkeletonColunmListVideos />
         </S.ContainerRight>
-      </S.Container2>
+      </S.Container>
     );
   }
 
   return (
-    <S.Container2>
-      <S.Container>
-        <S.ContainerLeft>
-          <S.ContainerPLayerVideo>
-            <ReactPlayer
-              className={'react-player'}
-              url={videoUrl}
-              light={videoThumbnail}
-              width="74vw"
-              height="auto"
-              controls={true}
-              playing={true}
-              playIcon={
-                <div
-                  style={{
-                    width: '100%',
-                    height: '100%',
-                    background: 'rgb(0, 0, 0, 0.5)',
-                    display: 'flex',
-                    alignContent: 'center',
-                    justifyContent: 'center'
-                  }}
-                >
-                  <button style={{ background: 'transparent', border: 'none' }}>
-                    <img
-                      src={'/assets/icon/icon-playVideo.svg'}
-                      alt="Play vídeo"
-                    />
-                  </button>
-                </div>
-              }
-            />
-            <S.WrapInfo>
+    <S.Container>
+      <S.ContainerLeft>
+        <S.ContainerPLayerVideo>
+          <ReactPlayer
+            className={'react-player'}
+            url={videoUrl}
+            light={videoThumbnail}
+            height="auto"
+            width="100%"
+            controls={true}
+            playing={true}
+            playIcon={
+              <div
+                style={{
+                  width: '100%',
+                  height: '100%',
+                  background: 'rgb(0, 0, 0, 0.5)',
+                  display: 'flex',
+                  alignContent: 'center',
+                  justifyContent: 'center'
+                }}
+              >
+                <button style={{ background: 'transparent', border: 'none' }}>
+                  <img
+                    src={'/assets/icon/icon-playVideo.svg'}
+                    alt="Play vídeo"
+                  />
+                </button>
+              </div>
+            }
+          />
+          <S.WrapInfo>
+            {videoApi ? (
               <S.Star>
-                <p>
-                  ★
-                  <br />
-                  Componente
-                  <br />
-                  estrela
-                </p>
+                <FavoriteButton video={videoApi} defaultColor="primary" />
               </S.Star>
-              <S.WrapTitleDescription>
-                <h1>{videoApi?.nome}</h1>
-                <h2>{videoApi?.descricao}</h2>
-              </S.WrapTitleDescription>
-            </S.WrapInfo>
-          </S.ContainerPLayerVideo>
-          <DivideLine />
-          <CommentList comments={comments} />
-        </S.ContainerLeft>
-      </S.Container>
+            ) : null}
+            <S.WrapTitleDescription>
+              <h1>{videoApi?.nome}</h1>
+              <h2>{videoApi?.descricao}</h2>
+              {videoApi?.dataPublicacao ? (
+                <time>{formataDate(videoApi?.dataPublicacao)}</time>
+              ) : null}
+            </S.WrapTitleDescription>
+          </S.WrapInfo>
+        </S.ContainerPLayerVideo>
+        <DivideLine />
+        <CommentList comments={comments} />
+      </S.ContainerLeft>
       <S.ContainerRight>
         <S.TitleRecommended>Vídeos Recomendados</S.TitleRecommended>
         <VideoRecomendedList videoRecomended={recommendedVideos} />
       </S.ContainerRight>
-    </S.Container2>
+    </S.Container>
   );
 };
--- src/pages/VideoClass/styles.ts
+++ src/pages/VideoClass/styles.ts
@@ -1,6 +1,7 @@
 import styled, { css } from 'styled-components';
 
 export const ContainerPLayerVideo = styled.div`
+  flex: 1;
   .react-player > div {
     background-repeat: no-repeat;
     background-size: contain !important;
@@ -16,9 +17,8 @@ export const ContainerPLayerVideo = styled.div`
 
 export const WrapInfo = styled.div`
   display: flex;
-  padding: ${({ theme }) => theme.spacing.small};
-  margin-top: 2rem;
-  margin-bottom: 1rem;
+  flex: 1;
+  padding-block: ${({ theme }) => theme.spacing.large};
 `;
 
 export const Star = styled.div`
@@ -26,17 +26,11 @@ export const Star = styled.div`
   padding-right: 1.5rem;
 `;
 
-// type StarButtonProps = {
-//   isFavorited: boolean;
-// };
-
-// export const StarButton = styled.div<StarButtonProps>`
 export const StarButton = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   width: 4rem;
-  /* height: 56%; */
   margin-left: -${({ theme }) => theme.spacing.small};
   margin-top: ${({ theme }) => theme.spacing.xxsmall};
 
@@ -56,56 +50,60 @@ export const StarButton = styled.div`
 `;
 
 export const WrapTitleDescription = styled.div`
-  width: 83%;
-  font-size: 1rem;
-  /* margin-top: ${({ theme }) => theme.spacing.medium}; */
-  overflow-wrap: break-word;
+  ${({ theme }) => css`
+    width: 83%;
+    font-size: 1rem;
+    overflow-wrap: break-word;
+    display: flex;
+    flex-direction: column;
+    gap: ${theme.spacing.xsmall};
 
-  h1 {
-    color: ${({ theme }) => theme.colors.primary};
-    font-weight: ${({ theme }) => theme.font.weight.semiBold};
-    margin-bottom: ${({ theme }) => theme.spacing.small};
-  }
+    h1 {
+      color: ${theme.colors.primary};
+      font-weight: ${theme.font.weight.semiBold};
+    }
 
-  h2 {
-    color: ${({ theme }) => theme.colors.primary};
-    font-weight: ${({ theme }) => theme.font.weight.regular};
-    font-size: ${({ theme }) => theme.font.sizes.small};
-  }
+    h2 {
+      color: ${theme.colors.primary};
+      font-weight: ${theme.font.weight.regular};
+      font-size: ${theme.font.sizes.small};
+    }
+
+    > time {
+      font-size: ${theme.font.sizes.xsmall};
+      color: ${theme.colors.gray};
+    }
+  `}
 `;
+
 export const Container = styled.div`
-  display: inline-flex;
-  /* width: 100%; */
-  margin-top: -4rem;
-`;
-
-export const Container2 = styled.div`
   display: flex;
-  width: 100%;
-  justify-content: space-between;
-  /* padding-block: 0 !important; */
-  margin-top: 10.2rem;
+  ${({ theme }) => css`
+    gap: ${theme.spacing.medium};
+    padding-top: ${theme.spacing.medium};
+    padding-left: ${theme.spacing.medium};
+  `}
 `;
 
 export const ContainerLeft = styled.div`
   display: flex;
+  flex: 1;
   flex-direction: column;
-  /* justify-content: space-between; */
-  width: 75vw;
+  padding-bottom: ${({ theme }) => theme.spacing.xxlarge};
 `;
 
 export const ContainerRight = styled.div`
-  width: 25vw;
-  margin-top: -4rem;
   background: #fbfbfe;
   box-shadow: -4px 0px 20px rgb(0 0 0 / 10%);
 `;
 
 export const TitleRecommended = styled.div`
-  color: ${({ theme }) => theme.colors.primary};
-  font-size: ${({ theme }) => theme.font.sizes.small};
-  font-weight: ${({ theme }) => theme.font.weight.semiBold};
-  text-align: center;
-  padding: ${({ theme }) => theme.spacing.medium};
-  background-color: #dadada;
+  ${({ theme }) => css`
+    text-align: center;
+    background-color: #dadada;
+    color: ${theme.colors.primary};
+    font-size: ${theme.font.sizes.small};
+    font-weight: ${theme.font.weight.semiBold};
+    padding: ${theme.spacing.medium};
+  `}
 `;
