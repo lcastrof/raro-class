cda104ac1f216c66ba3a637a0937ba1a89b0d237
Lucas de Castro <lucascastrofr@gmail.com>
2022-04-30 09:13:06 -0300

    refactor: ordena favoritos ao salvar

--- src/store/favorites.ts
+++ src/store/favorites.ts
@@ -1,6 +1,6 @@
 import create from 'zustand';
-import { Video } from '../pages/Home';
 import api from '../services/api';
+import { Video } from '../types/Video';
 
 type FavoritesHookProps = {
   favorites: Video[];
@@ -17,9 +17,18 @@ export const useFavorites = create<FavoritesHookProps>((set) => ({
   },
   addFavorite: async (videoData: Video) => {
     await api.post(`/videos/${videoData.id}/favoritos`);
-    set(({ favorites }) => ({
-      favorites: [...favorites, videoData]
-    }));
+    set(({ favorites }) => {
+      const updatedFavorites = [...favorites, videoData];
+      updatedFavorites.sort((a, b) => {
+        if (!a.dataPublicacao || !b.dataPublicacao) return 0;
+        if (a.dataPublicacao < b.dataPublicacao) return -1;
+        if (a.dataPublicacao > b.dataPublicacao) return 1;
+        return 0;
+      });
+      return {
+        favorites: updatedFavorites
+      };
+    });
   },
   removeFavorite: async (videoId: string) => {
     await api.delete(`/videos/${videoId}/favoritos`);
