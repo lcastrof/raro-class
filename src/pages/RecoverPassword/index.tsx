a85a7b2b5ede186444ce85503f4533980bfc4050
Lucas de Castro <lucascastrofr@gmail.com>
2022-05-01 19:40:26 -0300

    refactor: renomeia layout e aplica outlet

--- src/pages/RecoverPassword/index.tsx
+++ src/pages/RecoverPassword/index.tsx
@@ -1,9 +1,8 @@
 import { Input } from '../../components/Input';
 import { VscMail } from 'react-icons/vsc';
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
-import { PageMain } from '../../layouts/PageMain';
 import { PrimaryButton } from '../../components/PrimaryButton';
-import { MainTextLayout } from '../../layouts/MainTextLayout';
+import { DividedTextLayout } from '../../layouts/DividedTextLayout';
 import { LinkHome } from '../../components/LinkHome';
 import { FieldValues, useForm } from 'react-hook-form';
 import * as yup from 'yup';
@@ -36,51 +35,49 @@ export const RecoverPassword = () => {
       await api.post('/auth/solicitar-codigo', { email });
       toast.success('Código enviado com sucesso!');
       navigate('/novasenha');
-    } catch (error) {
-      console.log({ error });
+    } catch (err) {
+      console.log({ err });
       setErrorMessage('Erro ao enviar o código. Tente novamente mais tarde');
     }
   };
 
   return (
-    <PageMain>
-      <MainTextLayout
-        title="Digite o seu e-mail"
-        subTitle="para receber um código"
-        linkparagrafo1={{
-          url: '/login',
-          text: 'Fazer login'
-        }}
-        linkparagrafo2={{
-          url: '/novasenha',
-          text: 'Já tenho o código'
-        }}
-        icon={<MdKeyboardArrowLeft size={18} />}
-        iconRight={<MdKeyboardArrowRight size={18} />}
-        linkHome={<LinkHome />}
-      >
-        {errorMessage ? (
-          <S.ErrorMessageWrapper>
-            <S.ErrorMessage>{errorMessage}</S.ErrorMessage>
-          </S.ErrorMessageWrapper>
-        ) : null}
-        <form id="request-code-form" onSubmit={handleSubmit(handleRequestCode)}>
-          <Input
-            name="email"
-            register={register}
-            errors={errors.email}
-            placeholder="E-mail"
-            icon={<VscMail size={18} />}
-          />
-        </form>
-        <PrimaryButton
-          size="fullWidth"
-          text="Solicitar código"
-          type="submit"
-          form="request-code-form"
-          loading={isSubmitting}
+    <DividedTextLayout
+      title="Digite o seu e-mail"
+      subTitle="para receber um código"
+      linkparagrafo1={{
+        url: '/login',
+        text: 'Fazer login'
+      }}
+      linkparagrafo2={{
+        url: '/novasenha',
+        text: 'Já tenho o código'
+      }}
+      icon={<MdKeyboardArrowLeft size={18} />}
+      iconRight={<MdKeyboardArrowRight size={18} />}
+      linkHome={<LinkHome />}
+    >
+      {errorMessage ? (
+        <S.ErrorMessageWrapper>
+          <S.ErrorMessage>{errorMessage}</S.ErrorMessage>
+        </S.ErrorMessageWrapper>
+      ) : null}
+      <form id="request-code-form" onSubmit={handleSubmit(handleRequestCode)}>
+        <Input
+          name="email"
+          register={register}
+          errors={errors.email}
+          placeholder="E-mail"
+          icon={<VscMail size={18} />}
         />
-      </MainTextLayout>
-    </PageMain>
+      </form>
+      <PrimaryButton
+        size="fullWidth"
+        text="Solicitar código"
+        type="submit"
+        form="request-code-form"
+        loading={isSubmitting}
+      />
+    </DividedTextLayout>
   );
 };
